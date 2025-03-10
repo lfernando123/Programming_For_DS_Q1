f1c1c1f9ecc3634d01947403fca814feaa0cf95f
lfernando123 <lgithendra@yahoo.com>
2025-03-10 22:39:56 +0530

    Add Person class with attributes and methods for encapsulation and role duties

--- person.py
+++ person.py
@@ -1,1 +1,15 @@
+class Person:
+    def __init__(self, name, age, address, ssn):
+        self.name = name
+        self.age = age
+        self.address = address
+        self.__ssn = ssn  # Encapsulated attribute
 
+    def get_ssn(self):
+        return self.__ssn
+
+    def set_ssn(self, ssn):
+        self.__ssn = ssn
+
+    def role_duties(self):
+        return "General responsibilities of a school member."
